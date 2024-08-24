c9ee741e5e888191b6e30f4aed29bb8b32b31e01
deepshikhats <deepshikha.mec@gmail.com>
2024-08-24 22:49:45 +0530

    refactor: enhancement

--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -5,6 +5,7 @@ import { setIsValidUser, setUserCred } from '@/store/slices/authSlice';
 import authSchema from '@/utils/validationSchemas';
 import { Formik, FormikHelpers } from 'formik';
 import { useState } from 'react';
+import { Col, Container } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
@@ -30,155 +31,108 @@ const Auth = () => {
     }
   };
   return (
-    <div className="authBox">
-      <Formik
-        validationSchema={authSchema}
-        onSubmit={handleFormSubmit}
-        initialValues={{
-          userName: '',
-          password: '',
-          keepMeSignIn: false,
-        }}
-      >
-        {({ handleSubmit, handleChange, resetForm, values, errors }) => (
-          <Form noValidate onSubmit={handleSubmit} className="authBox_form">
-            <Row>
-              <h1>{isLogin ? 'Sign In' : 'Sign Up'}</h1>
-            </Row>
-            <Row className="newUser">
-              <span>
-                {isLogin ? 'New User? ' : 'Existing User? '}
-                <span
-                  className="link"
-                  onClick={() => {
-                    setIsLogin((cv) => !cv);
-                    resetForm();
-                  }}
+    <Container className="h-100 py-5">
+      <Row className="h-100">
+        <Col className="my-auto">
+          <div className="authBox">
+            <Formik
+              validationSchema={authSchema}
+              onSubmit={handleFormSubmit}
+              initialValues={{
+                userName: '',
+                password: '',
+                keepMeSignIn: false,
+              }}
+            >
+              {({ handleSubmit, handleChange, resetForm, values, errors }) => (
+                <Form
+                  noValidate
+                  onSubmit={handleSubmit}
+                  className="authBox_form"
                 >
-                  {isLogin ? 'Create an account' : 'Login In'}
-                </span>
-              </span>
-            </Row>
-            <Row className="mb-3 d-flex flex-column gap-3">
-              <Form.Group controlId="validationFormik01">
-                <Form.Control
-                  type="text"
-                  name="userName"
-                  value={values.userName}
-                  onChange={handleChange}
-                  // isValid={touched.userName && !errors.userName}
-                  className="inputBox"
-                />
+                  <Row>
+                    <h1>{isLogin ? 'Sign In' : 'Sign Up'}</h1>
+                  </Row>
+                  <Row className="newUser">
+                    <span>
+                      {isLogin ? 'New User? ' : 'Existing User? '}
+                      <span
+                        className="link"
+                        onClick={() => {
+                          setIsLogin((cv) => !cv);
+                          resetForm();
+                        }}
+                      >
+                        {isLogin ? 'Create an account' : 'Login In'}
+                      </span>
+                    </span>
+                  </Row>
+                  <Row className="mb-3 d-flex flex-column gap-3">
+                    <Form.Group controlId="validationFormik01">
+                      <Form.Control
+                        type="text"
+                        name="userName"
+                        value={values.userName}
+                        onChange={handleChange}
+                        // isValid={touched.userName && !errors.userName}
+                        className="inputBox"
+                      />
 
-                <Form.Control.Feedback type="invalid" className="d-block">
-                  {errors.userName}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="validationFormik02">
-                <Form.Control
-                  type="text"
-                  name="password"
-                  value={values.password}
-                  onChange={handleChange}
-                  // isValid={touched.password && !errors.password}
-                  className="inputBox"
-                />
+                      <Form.Control.Feedback type="invalid" className="d-block">
+                        {errors.userName}
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group controlId="validationFormik02">
+                      <Form.Control
+                        type="text"
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        // isValid={touched.password && !errors.password}
+                        className="inputBox"
+                      />
 
-                <Form.Control.Feedback type="invalid" className="d-block">
-                  {errors.password}
-                </Form.Control.Feedback>
-              </Form.Group>
+                      <Form.Control.Feedback type="invalid" className="d-block">
+                        {errors.password}
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                  </Row>
+                  {isLogin && (
+                    <Row>
+                      <Form.Group className="position-relative mb-3">
+                        <Form.Check
+                          name="keepMeSignIn"
+                          label="Keep me signed in"
+                          onChange={handleChange}
+                          checked={values.keepMeSignIn}
+                          isInvalid={!!errors.keepMeSignIn}
+                          feedback={errors.keepMeSignIn}
+                          feedbackType="invalid"
+                          id="validationFormik0"
+                        />
+                      </Form.Group>
+                    </Row>
+                  )}
+                  <Button className="w-100" type="submit">
+                    {isLogin ? 'Sign In' : 'Sign Up'}
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+            <div className="d-flex my-4 align-items-center gap-2">
+              <hr className="divider" />
+              <span>Or Sign In With</span>
+              <hr className="divider" />
+            </div>
+            <Row>
+              <Social />
             </Row>
-            {isLogin && (
-              <Row>
-                <Form.Group className="position-relative mb-3">
-                  <Form.Check
-                    name="keepMeSignIn"
-                    label="Keep me signed in"
-                    onChange={handleChange}
-                    checked={values.keepMeSignIn}
-                    isInvalid={!!errors.keepMeSignIn}
-                    feedback={errors.keepMeSignIn}
-                    feedbackType="invalid"
-                    id="validationFormik0"
-                  />
-                </Form.Group>
-              </Row>
-            )}
-            <Button className="w-100" type="submit">
-              {isLogin ? 'Sign In' : 'Sign Up'}
-            </Button>
-          </Form>
-        )}
-      </Formik>
-      <div className="d-flex my-4 align-items-center gap-2">
-        <hr className="divider" />
-        <span>Or Sign In With</span>
-        <hr className="divider" />
-      </div>
-      <Row>
-        <Social />
+          </div>
+        </Col>
+        <Col className="d-none d-lg-block">image</Col>
       </Row>
-    </div>
+    </Container>
   );
 };
 
 export default Auth;
-
-// import Button from 'react-bootstrap/Button';
-// import Col from 'react-bootstrap/Col';
-// import Form from 'react-bootstrap/Form';
-// import InputGroup from 'react-bootstrap/InputGroup';
-// import Row from 'react-bootstrap/Row';
-// import * as formik from 'formik';
-// import * as yup from 'yup';
-
-// function FormExample() {
-//   const { Formik } = formik;
-
-//   const schema = yup.object().shape({
-//     firstName: yup.string().required(),
-//     lastName: yup.string().required(),
-//     username: yup.string().required(),
-//     city: yup.string().required(),
-//     state: yup.string().required(),
-//     zip: yup.string().required(),
-//     terms: yup.bool().required().oneOf([true], 'Terms must be accepted'),
-//   });
-
-//   return (
-//     <Formik
-//       validationSchema={schema}
-//       onSubmit={console.log}
-//       initialValues={{
-//         firstName: 'Mark',
-//         lastName: 'Otto',
-//         username: '',
-//         city: '',
-//         state: '',
-//         zip: '',
-//         terms: false,
-//       }}
-//     >
-//       {({ handleSubmit, handleChange, values, touched, errors }) => (
-//         <Form noValidate onSubmit={handleSubmit}>
-//           <Form.Group className="mb-3">
-//             <Form.Check
-//               required
-//               name="terms"
-//               label="Agree to terms and conditions"
-//               onChange={handleChange}
-//               isInvalid={!!errors.terms}
-//               feedback={errors.terms}
-//               feedbackType="invalid"
-//               id="validationFormik0"
-//             />
-//           </Form.Group>
-//           {/* <Button type="submit">Submit form</Button> */}
-//         </Form>
-//       )}
-//     </Formik>
-//   );
-// }
-
-// export default FormExample;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,30 +1,25 @@
 import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import PrivateRoute from './privateRoutes.tsx/index.tsx';
 import PublicRoute from './publicRoutes/index.tsx';
 import PATH from './routePaths';
-import Layout from '@/components/layout/layout.tsx';
-import PrivateRoute from './privateRoutes.tsx/index.tsx';
 
 const Auth = lazy(() => import('@/pages/auth'));
 const Home = lazy(() => import('@/pages/home'));
 
 const Router = createBrowserRouter([
   {
-    element: <Layout />,
+    element: <PublicRoute />,
     ErrorBoundary: () => <div>Loading...</div>,
     children: [
       {
-        element: <PublicRoute />,
-        children: [
-          {
-            index: true,
-            path: PATH.auth,
-            element: <Auth />,
-          },
-        ],
+        index: true,
+        path: PATH.auth,
+        element: <Auth />,
       },
     ],
   },
+
   {
     element: <PrivateRoute />,
     children: [
